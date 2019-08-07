81b4fb3bc0f30fe367821a3ef710686f68e38146
Ilham Syahid S <ilhamsyahids@gmail.com>
2019-08-07 21:36:02 +0700

    update

--- randomizer.js
+++ randomizer.js
@@ -2,12 +2,19 @@
 // Ingat untuk cek availability string tersebut di database
 
 async function string() {
-  let charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // List huruf kapital
+  let setKapital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let randoms = '';
 
+  // Looping 4x untuk mendapatkan empat huruf
   for (let i = 0; i < 4; i++) {
-    let random = Math.floor(Math.random() * charSet.length);
-    randoms += charSet.substring(random, random + 1);
+    //   Math.random menghasilkan nilai desimal (0-1),
+    //   kalikan dengan banyaknya jumlah huruf,
+    //   lalu dibulatkan kebawah
+    let random = Math.floor(Math.random() * setKapital.length);
+
+    // Mengambil satu huruf
+    randoms += setKapital.substring(random, random + 1);
   }
   return randoms;
 }
