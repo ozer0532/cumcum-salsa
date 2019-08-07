61e531b5ff993e18747d3d7d99d00a178191d0c2
unknown <gunawan48160@gmail.com>
2019-08-07 21:22:59 +0700

    Menu function done, database edit

--- database.js
+++ database.js
@@ -1,25 +1,7 @@
-
-
-// Mengubah state pemesanan dari nama ke jumlah, jumlah ke alamat, dst.
-async function setStatePesan (context, newState) {
-    
+class Database {
+	
 }
 
-// Mengoutput status pemesanan dari user tersebut
-async function getStatePesan (context) {
-    let state;
-
-    return state;
-}
-
-// Menyimpan kode pemesanan pengguna ke database
-async function setKodePesan (context) {
-    
-}
-
-// Mengoutput id pengguna yang memiliki pemesanan yang sesuai dengan kode
-async function getUserPesan (kode) {
-    let userId;
-
-    return userId;
+module.exports = {
+    Database: Database
 }
--- menufunction.js
+++ menufunction.js
@@ -3,26 +3,20 @@
 
 async function produk (context) {
 
-	// Produk Greeting
 	context.reply([
+		// Produk Greeting
 		{
   			"type": "text",
   			"text": "Halo! Berikut produk yang dapat kamu beli dari Cumcum Salsa!"
-		}
-	]);
-
-	// Produk (Image 1)
-	context.reply([
+		},
+		// Produk (Image 1)
 		{
 		  "type": "image",
 		  "originalContentUrl": "https://i.imgur.com/p7cgjwV.jpg",
 		  "previewImageUrl": "https://i.imgur.com/p7cgjwV.jpg",
 		  "animated": false
-		}
-	]);
-
-	// Produk (Detail 1)
-	context.reply([
+		},
+		// Produk (Detail 1)
 		{
 		  "type": "text",
 		  "text": "Cumcum Salsa dengan isian rasa :\n1. Stroberi\n2. Pandan\n3. Coklat\n4..\n\nHarga Rp 35.000/ kotak"
@@ -31,45 +25,35 @@ async function produk (context) {
 }
 
 async function faq (context) {
-	
-	// FAQ Greeting
+
 	context.reply([
+		// FAQ Greeting
 		{
 		  "type": "text",
 		  "text": "Halo! Berikut pertanyaan-pertanyaan yang sering diajukan pengunjung Line Bot Cumcum Salsa ini. Siapa tahu ada pertanyaan kamu."
-		}
-	]);
-
-	// FAQ (Questions)
-	context.reply([
+		},
+		// FAQ (Questions)
 		{
 		  "type": "text",
 		  "text": "Q = Pertanyaan\nA = Jawaban\n\nQ : Ada berapa varian Cumcum Salsa?\nA : Untuk saat ini terdapat 1 varian Cumcum Salsa.\n\nQ : Untuk pengiriman dilakukan dengan kurir apa?\nA : Pengiriman dilakukan dengan kurir JNE Reguler.\n\nQ : Berapa lama produk akan sampai setelah konfirmasi?\nA : Kira-kira 3 sampai 5 hari.\n\nQ : Apa pengiriman dapat dipercepat?\nA : Untuk pengiriman express dapat mengontak langsung line Customer Service Cumcum Salsa dengan id CSCumcumSalsa."
-		}
-	]);
-
-	// FAQ (Penutup)
-	context.reply([
+		},
+		// FAQ (Penutup)
 		{
 		  "type": "text",
 		  "text": "Sekian pertanyaan yang sering ditanyakan pengguna bot line Cumcum Salsa. \nJika pertanyaan kamu tidak ada di atas, dapat langsung mengontak line Customer Service Cumcum Salsa dengan id @CSCumcumSalsa."
 		}
 	]);
-    
 }
 
 async function testimoni (context) {
     
-    // Testimoni Greeting
     context.reply([
+    	// Testimoni Greeting
 		{
 		  "type": "text",
 		  "text": "Halo! Berikut testimoni dari pelanggan-pelanggan yang puas akan produk Cumcum Salsa."
-		}
-	]);
-
-    // Testimoni (Image 1)
-	context.reply([
+		},
+		//Testimoni (Image 1)
 		{
 		  "type": "image",
 		  "originalContentUrl": "https://i.imgur.com/n3m61Hg.jpg",
