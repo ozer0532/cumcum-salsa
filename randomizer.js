7cf65b6d43a7f72eb220581f8a8f9f42cf9ba140
Ilham Syahid S <ilhamsyahids@gmail.com>
2019-08-07 21:17:58 +0700

    random

--- randomizer.js
+++ randomizer.js
@@ -1,8 +1,13 @@
 // Gunakan fungsi ini untuk mengoutput string dengan 4 karakter random
 // Ingat untuk cek availability string tersebut di database
 
-async function string () {
-    let output;
+async function string() {
+  let charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randoms = '';
 
-    return output
+  for (let i = 0; i < 4; i++) {
+    let random = Math.floor(Math.random() * charSet.length);
+    randoms += charSet.substring(random, random + 1);
+  }
+  return randoms;
 }
