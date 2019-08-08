3ed11fd53278a39db5480cd5f61fbc756dc782b5
ozer0532itb <ozer0532@students.itb.ac.id>
2019-08-08 17:08:27 +0700

    Added push messages

--- clientpushmessage.js
+++ clientpushmessage.js
@@ -1,15 +1,24 @@
 // Gunakan pushAPI.push(userID, [ListPesanJSON]) untuk mengirimkan pesan ke user tersebut
 
 async function sudahFixed (dataPesan) {
-
+await pushAPI.push(userID, [{
+    "type": "text",
+    "text": "[FIXED]\nNama pemesan : " + dataPesan.nama + "\nJumlah pesanan : " + dataPesan.jumlah + "\nAlamat tujuan : " + dataPesan.alamat + "\nKontak pemesan : " + dataPesan.kontak + "\nPakai bubble wrap? : " + dataPesan.bubble + "\nHarga total : Rp " + dataPesan.hargaTotal + "\nPilihan pembayaran : " + dataPesan.pilihanPembayaran + "\nNomor pesanan : " + dataPesan.nomorPesanan
+  }])
 }
 
 async function sedangPacking (pushAPI, userID, dataPesan) {
-
+await pushAPI.push(userID, [{
+        "type": "text",
+        "text": "Halo " + dataPesan.nama + "!\nPesanan kamu telah dikonfirmasi dan sedang dalam proses packing.\nMohon menunggu 3 - 5 hari kerja untuk pengiriman pesanan.\nTerima kasih telah membeli produk Cumcum Salsa!"
+    }])
 }
 
-async function sedangPengiriman (pushAPI, dataPesan) {
-
+async function sedangPengiriman (pushAPI, userID, dataPesan) {
+await pushAPI.push(userID, [{
+    "type": "text",
+    "text": "Halo " + dataPesan.nama + "!\nPesanan kamu sedang dalam pengiriman.\nTerima kasih banyak telah membeli produk Cumcum Salsa."
+  }])
 }
 
 module.exports = {
